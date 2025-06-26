9ec6d7aa0965ecf471dc6f8c110d39821285e513
khang200923 <92591231+khang200923@users.noreply.github.com>
2025-06-26 15:40:32 +0700

    game mechanics

--- src/game/game.py
+++ src/game/game.py
@@ -27,7 +27,10 @@ class Game:
         self.whiteq_board[0, 4] = 1
         self.blackq_board[7, 3] = 1
 
-    def get_state(self, turn: bool) -> GameState:
+    def get_state(self, turn: bool | None = None) -> GameState:
+        if turn is None:
+            turn = self.is_white_turn
+
         blockers_board_tensor = torch.tensor(self.blockers_board, dtype=torch.float32, device=self.device)
         whiteq_board_tensor = torch.tensor(self.whiteq_board, dtype=torch.float32, device=self.device)
         blackq_board_tensor = torch.tensor(self.blackq_board, dtype=torch.float32, device=self.device)
@@ -45,3 +48,59 @@ class Game:
             oppq_board=blackq_board_tensor if turn else whiteq_board_tensor,
             is_self_turn=is_white_turn_tensor
         )
+
+    def make_move(self, move: tuple[int, int], turn: bool | None = None):
+        assert self.result() is None, "Game is already over!"
+
+        if turn is None:
+            turn = self.is_white_turn
+
+        assert turn == self.is_white_turn, "It's not your turn!"
+
+        state = self.get_state(turn)
+        current_queen_pos = state.find_queen_position()
+        legal_moves_mask = state.mask_legal_moves()
+        assert legal_moves_mask[move], "Illegal move!"
+
+        self.blockers_board[current_queen_pos] = 1
+        if turn:
+            self.whiteq_board = np.zeros((8, 8), dtype=np.int32)
+            self.whiteq_board[move] = 1
+            self.blackq_board[move] = 0
+        else:
+            self.blackq_board = np.zeros((8, 8), dtype=np.int32)
+            self.blackq_board[move] = 1
+            self.whiteq_board[move] = 0
+        self.is_white_turn = not self.is_white_turn
+
+    def agent_move(self) -> tuple[np.ndarray, tuple[int, int]]:
+        state = self.get_state()
+        model = self.white if self.is_white_turn else self.black
+
+        with torch.no_grad():
+            action_probs = model(state.selfq_board, state.oppq_board, state.blockers_board)
+        action_probs = action_probs.cpu().numpy()
+        legal_moves_mask = state.mask_legal_moves().cpu().numpy()
+        legal_action_probs = action_probs * legal_moves_mask.astype(np.float32)
+        try:
+            legal_action_probs /= legal_action_probs.sum()
+        except ZeroDivisionError:
+            legal_action_probs.fill(1.0 / legal_moves_mask.sum())
+
+        move = np.random.choice(np.arange(64), p=legal_action_probs.flatten())
+        move = (move // 8, move % 8)
+        if not self.is_white_turn:
+            move = (7 - move[0], move[1]) # Flip again because of perspective
+        self.make_move(move)
+
+        return action_probs, move
+
+    def result(self) -> bool | None:
+        state = self.get_state()
+        if (state.selfq_board == 1).sum() == 0:
+            return False
+        if (state.oppq_board == 1).sum() == 0:
+            return True
+        if not state.mask_legal_moves().any():
+            return not self.is_white_turn # suffocation -> ded
+        return None
--- src/game/state.py
+++ src/game/state.py
@@ -9,3 +9,43 @@ class GameState:
     selfq_board: torch.Tensor
     oppq_board: torch.Tensor
     is_self_turn: torch.Tensor
+
+    def find_queen_position(self) -> tuple[int, int]:
+        assert (self.selfq_board == 1).sum() == 1, "There should be exactly one queen on the self board."
+        assert (self.selfq_board == 0).sum() == 63, "There should be no other queens on the self board."
+        queen_pos = torch.nonzero(self.selfq_board, as_tuple=False).squeeze(0)
+        res = (queen_pos[0].item(), queen_pos[1].item())
+        assert isinstance(res[0], int) and isinstance(res[1], int), "Queen position should be integers."
+        return res # type: ignore
+
+    def mask_legal_moves(self) -> torch.Tensor:
+        mask = torch.zeros_like(self.selfq_board, dtype=torch.bool)
+        selfq_pos = self.find_queen_position()
+        selfq_row, selfq_col = selfq_pos
+
+        directions = [
+            (1, 0),
+            (-1, 0),
+            (0, 1),
+            (0, -1),
+            (1, 1),
+            (1, -1),
+            (-1, 1),
+            (-1, -1)
+        ]
+
+        for dr, dc in directions:
+            r, c = selfq_row, selfq_col
+            r += dr
+            c += dc
+            while 0 <= r < 8 and 0 <= c < 8:
+                if self.blockers_board[r, c] == 1:
+                    break
+                if self.oppq_board[r, c] == 1:
+                    mask[r, c] = True
+                    break
+                mask[r, c] = True
+                r += dr
+                c += dc
+
+        return mask
